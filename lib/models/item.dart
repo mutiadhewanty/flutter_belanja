68584481136523a7c8532aedcf3e3395fc2e7625
mutiadhewanty <mutiadhewanty@gmail.com>
2022-03-22 22:31:17 +0700

    item fitur

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,6 +1,7 @@
 class Item {
   String name;
   int price;
+  String image;
 
-  Item({required this.name, required this.price});
+  Item({required this.name, required this.price, required this.image});
 }
